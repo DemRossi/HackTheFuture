ed98b8db6338d28f637c7da6df4c871d527f60a3
Wesley Wijsen <wesleywijsen@hotmail.com>
2020-11-24 14:10:51 +0100

    Put room part in comment

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,9 +3,20 @@ const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
-const name = prompt('What is your name?');
-appendMessage(`you joined.`);
-socket.emit('new-user', name);
+if (messageForm != null) {
+    const name = prompt('What is your name?');
+    appendMessage(`you joined.`);
+    socket.emit('new-user', name);
+
+    messageForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const message = messageInput.value;
+        socket.emit('send-chat-message', message);
+        messageInput.value = '';
+
+        appendMessage(`You : ${message}`);
+    });
+}
 
 // console.log('test');
 socket.on('chat-message', (data) => {
@@ -19,14 +30,6 @@ socket.on('user-connected', (data) => {
 socket.on('user-disconnected', (data) => {
     console.log(data);
     appendMessage(`${data} disconnected.`);
-});
-messageForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const message = messageInput.value;
-    socket.emit('send-chat-message', message);
-    messageInput.value = '';
-
-    appendMessage(`You : ${message}`);
 });
 
 function appendMessage(message) {
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,28 @@ var express = require('express');
 const app = require('../app');
 var router = express.Router();
 
-const rooms = { name: {} };
+const rooms = {};
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Webtech Game', rooms: rooms });
+    res.render('room', { title: 'Webtech Game', rooms: rooms });
 });
 // GET Room
-router.get('/:room', (req, res) => {
-    res.render('room', { roomName: req.params.room });
-});
+// router.get('/:room', (req, res) => {
+//     if (rooms[req.body.room] == null) {
+//         return res.redirect('/');
+//     }
+//     res.render('room', { roomName: req.params.room });
+// });
+// POST Room
+// router.post('/room', (req, res) => {
+//     if (rooms[req.body.room] != null) {
+//         return res.redirect('/');
+//     }
+//     rooms[req.body.room] = { users: {} };
+//     res.redirect(req.body.room);
+//     // send message on socket
+//     io.emit('room-created', req.body.room);
+// });
 
 module.exports = router;
