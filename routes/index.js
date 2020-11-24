51c687f6cc36b4f9f227cb7fcacb03ceaca9db3a
Wesley Wijsen <wesleywijsen@hotmail.com>
2020-11-24 13:23:41 +0100

    changed pug to ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const app = require('../app');
 var router = express.Router();
+
+const rooms = { name: {} };
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Webtech Game' });
+    res.render('index', { title: 'Webtech Game', rooms: rooms });
+});
+// GET Room
+router.get('/:room', (req, res) => {
+    res.render('room', { roomName: req.params.room });
 });
 
 module.exports = router;
